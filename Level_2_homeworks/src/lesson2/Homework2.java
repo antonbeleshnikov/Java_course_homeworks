7c1c0bcf844f666a40b62a2ea30b1874d5cbf070
Anton Beleshnikov <antonbeleshnikov@yandex.ru>
2021-08-06 08:04:39 +0300

    5-е задание

--- Level_2_homeworks/src/lesson2/Homework2.java
+++ Level_2_homeworks/src/lesson2/Homework2.java
@@ -3,10 +3,10 @@ package lesson2;
 public class Homework2 {
     public static void main(String[] args) {
         String[][] array = {
-                {"1","1","1","1"},
-                {"1","1","1","1"},
-                {"1","rdsjdjs1","1","1"},
-                {"1","1","1","1"}
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "rdsjdjs1", "1", "1"},
+                {"1", "1", "1", "1"}
         };
         System.out.println(arrayProcessing(array));
     }
@@ -16,19 +16,18 @@ public class Homework2 {
             throw new MyArraySizeException();
         }
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length !=4 ) {
+            if (arr[i].length != 4) {
                 throw new MyArraySizeException();
             }
         }
-        int sum=0, parsed_number;
-        for (int i=0; i<arr.length;i++) {
-            for (int j=0; j<arr[i].length;j++) {
+        int sum = 0, parsed_number;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 try {
                     parsed_number = Integer.parseInt(arr[i][j]);
-                    sum+= parsed_number;
-                }
-                catch (NumberFormatException e) { // Я так и не понял, почему если написать здесь catch MyArrayDataException, это не будет работать, выбрасываться все равно будет исключение NumberFormatException
-                    throw new MyArrayDataException(i+1,j+1);
+                    sum += parsed_number;
+                } catch (NumberFormatException e) { // Я так и не понял, почему если написать здесь catch MyArrayDataException, это не будет работать, выбрасываться все равно будет исключение NumberFormatException
+                    throw new MyArrayDataException(i + 1, j + 1);
                 }
             }
         }
